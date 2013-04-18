7ace64e8605a0d162a72f808cdc4002e8d6ab053
Ralf Eriksson <flar@passagen.se>
2013-04-18 21:25:02 +0200

    Blog Videoversionen klar anvisning DB login

---
+++ resources/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_USER',     'user');                    // <-- mysql db user
+define('DB_PASSWORD', 'secret password');        // <-- mysql db password
+define('DB_DATABASE', 'dbname');                 // <-- mysql db name
+define('DB_HOST',     'serverhost');            // <-- mysql server host
+
+?>
